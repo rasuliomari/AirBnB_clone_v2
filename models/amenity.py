c6f607f6aca143cd9204c5a9dd1a92b6c5d01bfa
rasuliomari <rasuli707046@gmail.com>
2023-11-22 14:45:04 -0800

    task10

--- models/amenity.py
+++ models/amenity.py
@@ -1,15 +1,17 @@
-"""Defines the Amenity class."""
-from models.base_model import Base
-from models.base_model import BaseModel
-from sqlalchemy import Column
-from sqlalchemy import String
-from sqlalchemy.orm import relationship
+#!/usr/bin/python3
+"""This is the amenity class"""
+from sqlalchemy import Column, Integer, String, ForeignKey, MetaData
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship, backref
+from models.place import place_amenity
 
 
 class Amenity(BaseModel, Base):
-    """Represents an Amenity for a MySQL database
+    """This is the class for Amenity
+    Attributes:
+        name: input name
     """
+
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary="place_amenity",
-                                   viewonly=False)
+    place_amenities = relationship("Place", secondary=place_amenity)
